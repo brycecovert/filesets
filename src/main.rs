b935d9ae0ee7dd190c06943592f83d8cc6b4ef12
Bryce Covert <bryce@pelo.tech>
2019-04-21 07:52:32 -0700

    calculates giant hashes.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 extern crate clap;
+extern crate crypto;
 use clap::{Arg, App, SubCommand};
-use tokio_fs::{read_dir};
+use std::fs::{read_dir, read};
 use std::path::Path;
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
+
+fn printMd5(path: &Path) -> i32{
+    let mut x = 0;
+    for entry in read_dir(&path).unwrap() {
+        let p = entry.unwrap().path();
+        if (p.is_file()) {
+            let filename = p.to_str().unwrap();
+            let bytes = read(&p).unwrap();
+            let mut m = Md5::new();
+            m.input(&bytes);
+            x = x + 1;
+        } else if (p.is_dir()) {
+            x = x + printMd5(&p);
+        }
+    }
+    return x
+}
 
 
 fn main() {
@@ -18,9 +39,8 @@ fn main() {
              .takes_value(true))
         .get_matches();
         let path = Path::new(matches.value_of("left").unwrap());
-        for entry in read_dir(&path).unwrap() {
-            println!("filse : {}", entry.unwrap().path().to_str().unwrap());
-        }
+    let cnt = printMd5(&path);
+    println!("{}", cnt);
 }
 
 
