e99616d0d05e8ed5581818e6186548793a5c027f
Bryce Covert <bryce@pelo.tech>
2019-04-30 19:26:04 -0700

    fewer allocations

--- src/main.rs
+++ src/main.rs
@@ -42,9 +42,8 @@ fn reduce_hash_map(rx: &Receiver<Hashed>, cnt: usize) -> HashMap<String, Vec<Str
     rx.iter().take(cnt).fold(HashMap::new(), |mut a, z| {
         match z {
             Hashed::Res(x, y) => {
-                a.entry(x)
-                    .and_modify(|result| result.push(y.clone()))
-                    .or_insert(vec!(y.clone()));
+                let e = a.entry(x);
+                e.or_insert(vec!()).push(y);
                 a
             }
         }
@@ -74,39 +73,40 @@ fn main() {
     let pool = ThreadPool::new(12);
     let (tx, rx) = channel();
 
-    let prints = matches.values_of("print").unwrap().into_iter().map(|p| p.to_string()).collect::<HashSet<String>>();
+    let prints = matches.values_of("print").unwrap().into_iter().collect::<HashSet<&str>>();
 
     if let Some(directories)  = matches.values_of("directory") {
-        let directories = directories.into_iter().map(|x| x.to_string()).collect::<Vec<String>>();
-        let directories2 = directories.into_iter().collect::<Vec<String>>();
-        let directory_hashes: HashMap<String, HashMap<String, Vec<String>>> = directories.iter().map(|d| {
+        let directories = directories.into_iter().collect::<Vec<&str>>();
+
+        let directory_hashes: HashMap<&str, HashMap<String, Vec<String>>> = directories.iter().map(|d| {
             let cnt = walk(&Path::new(&d), &pool, tx.clone()).unwrap();
             let h = reduce_hash_map(&rx, usize::from(cnt));
             println!("{} has {} files", d, cnt);
-            (d.clone(), h)
+            (*d, h)
         }
         )
-            .collect::<HashMap<String, HashMap<String, Vec<String>>>>();
-        let mut locations = HashMap::<&String, &String>::new(); 
-        let mut seen = HashSet::<&String>::new(); 
+            .collect();
+        let mut locations = HashMap::<&str, &str>::new(); 
+        let mut seen = HashSet::<&str>::new(); 
         for directory in directories.iter() {
-            let others = directories.iter().filter(|d| *d != directory)
-                .flat_map(|d| directory_hashes.get(d).unwrap().keys())
-                .collect::<HashSet<&String>>();
+            let others: HashSet<_> = directories.iter().filter(|d| *d != directory)
+                .flat_map(|d| directory_hashes.get(*d).unwrap().keys())
+                .map(|d| d.as_str())
+                .collect();
 
             let mine_map = directory_hashes.get(directory).unwrap();
-            let mine = mine_map.keys().collect::<HashSet<&String>>();
+            let mine: HashSet<&str> = mine_map.keys().map(|x| x.as_str()).collect();
 
             if prints.contains("uniques") {
-                for result in mine.difference(&others).flat_map(|h| directory_hashes.get(directory).unwrap().get(h.clone()).unwrap()) {
+                for result in mine.difference(&others).flat_map(|h| mine_map.get(h.clone()).unwrap()) {
                     println!("{} is unique", result);
                 }
             }
 
             if prints.contains("duplicates") {
                 for result in mine.intersection(&others) {
-                    for duplicate in mine_map.get(&result.to_string()).unwrap() {
-                        if let Some(l) = locations.get(result) {
+                    for duplicate in mine_map.get(*result).unwrap() {
+                        if let Some(l) = locations.get(*result) {
                             println!("{} -> {}", duplicate, l);
                         }
                     }
@@ -115,8 +115,8 @@ fn main() {
 
             if prints.contains("rolling-duplicates") {
                 for result in mine.intersection(&seen) {
-                    for duplicate in mine_map.get(&result.to_string()).unwrap() {
-                        if let Some(l) = locations.get(result) {
+                    for duplicate in mine_map.get(*result).unwrap() {
+                        if let Some(l) = locations.get(*result) {
                             println!("{} -> {}", duplicate, l);
                         }
                     }
@@ -125,15 +125,14 @@ fn main() {
 
             if prints.contains("rolling-uniques") {
                 for result in mine.difference(&seen) {
-                    
-                    for unique in mine_map.get(&result.to_string()).unwrap() {
+                    for unique in mine_map.get(*result).unwrap() {
                         println!("{} is rolling-unique", unique);
                     }
                 }
             }
 
             for file in mine.iter() {
-                let filename = directory_hashes.get(directory).unwrap().get(file.clone()).unwrap().get(0).unwrap();
+                let filename = mine_map.get(file.clone()).unwrap().get(0).unwrap();
                 locations.entry(file).or_insert(filename);
                 seen.insert(file);
             }
